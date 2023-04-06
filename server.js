f4ba090208eddd6d7a8465d0604d9626142ae6ef
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-04-06 08:13:50 -0700

    added login functions and introduced middleware for passport

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+var app = express();
+app.use(require('serve-static')(__dirname + '/../../public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'sleepy head', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
