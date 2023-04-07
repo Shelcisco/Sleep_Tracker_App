3bcd4d5a09666440dfd5c2d8984f78f855ee7465
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-04-06 20:10:29 -0700

    changed passport over to local

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ const loginFormHandler = async (event) => {
     event.preventdefault();
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-
+      
     if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
--- server.js
+++ server.js
@@ -7,3 +7,15 @@ app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'sleepy head', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (!user.verifyPassword(password)) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
