d7bb8cbfa79991109b25020d9b7a83349e404327
isaacp5454 <118407259+isaacp5454@users.noreply.github.com>
2023-04-11 18:46:34 -0700

    added models

---
+++ models/Sleep.js
@@ -0,0 +1,29 @@
+const {Model, DataTypes}= require('sequelize');
+const sequelize = require('../config/connection');
+
+class Sleep extends Model {}
+
+
+
+Sleep.init(
+    {
+        id:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        hours_of_sleep:{
+            type:DataTypes.FLOAT,
+            allowNull:false,
+        },
+        your_mood:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        rem_sleep:{
+            type:DataTypes.FLOAT,
+            allowNull:false,
+        }
+    }
+);
+
+module.exports=Sleep;
