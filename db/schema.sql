7d581f671d3b057a67b63521261f9469d8d6276d
isaacp5454 <118407259+isaacp5454@users.noreply.github.com>
2023-04-13 20:22:18 -0700

    connected user/sleep table

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,9 @@ CREATE DATABASE users_db;
 
 USE sleep_sync_db
 
-CREATE EXTENSION IF NOT EXISTS  "uuid-ossp";
 
 CREATE TABLE users(
-    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     email_address VARCHAR(30)NOT NULL,
@@ -14,8 +13,14 @@ CREATE TABLE users(
 );
 
 CREATE TABLE sleep_data(
-  sleep_data_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  sleep_data_id INT AUTO_INCREMENT PRIMARY KEY,
   hours_of_sleep VARCHAR(30) NOT NULL,
   mood VARCHAR(30) NOT NULL,
   rem_sleep INT NOT NULL
 );
+
+ALTER TABLE users
+ADD COLUMN sleep_data_id INT NOT NULL,
+ADD CONSTRAINT fk_sleep_data_id
+FOREIGN KEY (sleep_data_id) REFERENCES sleep_data(sleep_data_id);
+
--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const {Model, DataTypes}= require('sequelize');
 const sequelize = require('../config/connection');
+const User = require('./User');
+const Sleep = require('./Sleep');
 
-class 
+ //sleep data with each other. we need to associate every sleep data with every user. 
+
+ User.associate = function(models){
+    Sleep.belongsTo(models.User,{
+      foreignKey:'id'
+    })
+  }
+  module.exports={
+    User,Sleep
+  };
