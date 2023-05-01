cff8a6c922b9da75e5af0124a69aee64ad1036f2
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-04-30 21:17:35 -0700

    add bcrypt

--- models/Sleep.js
+++ models/Sleep.js
@@ -1,47 +1,63 @@
-const {Model, DataTypes}= require('sequelize');
+const { Model, DataTypes }= require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Sleep extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+}
 
-Sleep.init(
+User.init(
     {
         id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
             primaryKey: true,
+            autoIncrement: true, 
+            allowNull: false    
         },
-        date: {
+        first_name: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        hours: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        mood: {
+        last_name: { 
             type: DataTypes.STRING,
             allowNull: false,
         },
-        rem_sleep: {
-            type: DataTypes.INTEGER,
+        email: {
+            type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
         },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-              model: 'users',
-              key: 'id',
-            },
-          },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len:[8]
+            }
+        }
     },
     {
+        hooks: {
+          beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          },
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'sleep',
+        modelName: 'users',
       }
 );
 
-module.exports=Sleep;
+
+module.exports = User;
