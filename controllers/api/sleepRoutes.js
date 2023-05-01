dcd17a1b71fe2ab7609588c3b703d04b168576bb
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-04-30 20:58:57 -0700

    Fixing Get/Post

--- controllers/api/sleepRoutes.js
+++ controllers/api/sleepRoutes.js
@@ -2,74 +2,81 @@ const router = require("express").Router();
 const { User, Sleep } = require("../../models");
 const withAuth = require("../../helpers/sqlHelpers");
 
-router.get("/", withAuth, async (req, res) => {
-  try {
-    const sleepData = new Sleep({
-      inlcude: [{ model: User, attributes: ["id", "first_name"] }],
-    });
+// router.get('/', withAuth, async (req, res) => {
+//     try {
+//         const userData = await User.findByPk(req.session.user_id, {
+//           attributes: { exclude: ['password'] },
+//           include: [{ model: Sleep }],
+//         });
 
-    const sleep = sleepData.get({ plain: true });
+//         const user = userData.get({ plain: true });
 
-    res.render("sleep", {
-      sleep,
-      logged_in: true,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status().json(err);
-  }
-});
-
-router.post("/", withAuth, async (req, res) => {
-  try {
-    const newSleepData = await Sleep.create({
-      date: req.body.date,
-      hours: req.body.hours,
-      mood: req.body.mood,
-      rem_sleep: req.body.rem_sleep,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newSleepData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.get('/', withAuth, async (req, res) => {
+//         res.render('sleep', {
+//           ...user,
+//           logged_in: true
+//         });
+//       } catch (err) {
+//         res.status(500).json(err);
+//       }
+// });
+router.get("/:id", async (req, res) => {
     try {
-        const userData = await User.findByPk(req.session.user_id, {
-          attributes: { exclude: ['password'] },
-          include: [{ model: Sleep }],
+        const newSleepData = await Sleep.findByPk(req.params.id, {
+            include: [{ model: User }],
         });
-
-        const user = userData.get({ plain: true });
-
-        res.render('sleep', {
-          ...user,
-          logged_in: true
-        });
-      } catch (err) {
-        res.status(500).json(err);
-      }
+        if (!newSleepData) {
+            res.status(404).json({ message: "No sleep data with that ID" });
+            return;
+        }
+        res.status(200).json(newSleepData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
-
 router.get("/", async (req, res) => {
     try {
-      const sleepData =  Sleep.findAll({
-        inlcude: [{ model: User, attributes: ["id", "first_name"] }],
-      });
-  
-      const sleep = sleepData.get({ plain: true });
-  
-      res.render("sleep", {
-        sleep,
-        logged_in: true,
-      });
+        const newSleepData = await Sleep.findAll({
+            include: [{ model: User }],
+        });
+        res.status(200).json(newSleepData);
     } catch (err) {
-      console.log(err);
-      res.status().json(err);
+        res.status(400).json(err);
     }
-  });
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const dateData = req.body.date;
+        const hoursData = req.body.hours;
+        const moodData = req.body.mood;
+        const rem_sleepData = req.body.rem_sleep;
+
+        const newSleepData = await Sleep.create({
+            date: dateData,
+            hours: hoursData,
+            mood: moodData,
+            rem_sleep: rem_sleepData,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newSleepData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+        const newSleepData = await Sleep.findByPk(req.params.id);
+        if (!newSleepData) {
+            res.status(404).json({ message: "No sleep data with that ID" });
+            return;
+        }
+        newSleepData.destroy();
+        res.status(200).json(newSleepData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
